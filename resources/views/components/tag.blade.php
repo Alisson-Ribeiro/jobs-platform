82e4e77f64f6b95c3af9ccc99f2dae28333ff71e
Alisson-Ribeiro <alisson-ohkamy@hotmail.com>
2024-05-28 10:38:26 -0300

    :lipstick: Feat: More style and blade components

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,3 +1,5 @@
-<a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">
+@props(['size' => 'base'])
+
+<a href="#" class="bg-white/10 hover:bg-white/25 px-3 py-1 rounded-xl text-xs font-bold transition-colors duration-300">
     {{ $slot }}
 </a>

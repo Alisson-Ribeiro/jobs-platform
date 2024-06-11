b3c637fcbb90c83c54f695d80ea0fd4fa59862ed
Alisson-Ribeiro <alisson-ohkamy@hotmail.com>
2024-06-11 17:55:34 -0300

    :recycle: refactor: turning layout dynamic

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,19 +1,20 @@
-<div class="p-4 bg-white/5 rounded-xl flex gap-x-6 border border-transparent hover:border-blue-800 group">
+@props(['job'])
+<x-panel class="flex gap-x-6">
     <div>
         <x-employer-logo/>
     </div>
 
     <div class="flex-1 flex flex-col">
-        <a href="#" class="self-start text-sm text-gray-400">Laracast</a>
+        <a href="#" class="self-start text-sm text-gray-400">{{ $job->employer->name }}</a>
 
-        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-600">Video Producer</h3>
-        <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000 USD</p>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-600">{{ $job->title }}</h3>
+        <p class="text-sm text-gray-400 mt-auto">{{ $job->salary }}</p>
 
     </div>
 
     <div>
-        <x-tag>Tag</x-tag>
-        <x-tag>Tag</x-tag>
-        <x-tag>Tag</x-tag>
+        @foreach ($job->Tags as $tag)
+            <x-tag :tag="$tag"></x-tag>
+        @endforeach
     </div>
-</div>
+</x-panel>
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,16 +1,16 @@
-<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center border border-transparent hover:border-blue-800 group">
-    <div class="self-start text-sm">Laracast</div>
+@props(['job'])
+<x-panel class="flex flex-col text-center">
+    <div class="self-start text-sm">{{ $job->employer->name }}</div>
     <div class="py-8">
-        <h3 class="group-hover:text-blue-600 text-xl font-bold">Video Producer</h3>
-        <p class="text-sm mt-4">Full Time - From $60,000 USD</p>
+        <h3 class="group-hover:text-blue-600 text-xl font-bold">{{ $job->title }}</h3>
+        <p class="text-sm mt-4">{{ $job->salary }}</p>
     </div>
     <div class="flex justify-between items-center mt-auto">
-        <div class="space-x-1">
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
+        <div>
+            @foreach ($job->Tags as $tag)
+                <x-tag :tag="$tag" size="small"></x-tag>
+            @endforeach
         </div>
-
         <x-employer-logo :width="42"/>
     </div>
-</div>
+</x-panel>

094dff037e444234ba16bb63c602f95907465a19
Alisson-Ribeiro <alisson-ohkamy@hotmail.com>
2024-06-27 19:20:45 -0300

    :recycle: refactor: redirecting jobs listing

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -2,7 +2,11 @@
 <x-panel class="flex flex-col text-center">
     <div class="self-start text-sm">{{ $job->employer->name }}</div>
     <div class="py-8">
-        <h3 class="group-hover:text-blue-600 text-xl font-bold">{{ $job->title }}</h3>
+        <h3 class="group-hover:text-blue-600 text-xl font-bold">
+            <a href=" {{ $job->url }} " target="_blank">
+                {{ $job->title }}
+            </a>
+        </h3>
         <p class="text-sm mt-4">{{ $job->salary }}</p>
     </div>
     <div class="flex justify-between items-center mt-auto">
